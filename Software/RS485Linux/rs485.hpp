30aace5f8e9f663284f0bd7433ce173946d86930
KasperSK <raizzat@gmail.com>
2015-04-15 11:41:28 +0200

    Tilføjet RS485 klasse
    Foreløblig klasse til at sende og modtage på RS485 bussen

---
+++ Software/RS485Linux/rs485.hpp
@@ -0,0 +1,64 @@
+#ifndef RS485_H_
+#define RS485_H_
+
+#include <termios.h>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+
+#define BUFFER_SIZE 50
+#define DEVKIT8000
+
+enum {
+	MSG_POS_RX_ADDR = 0,
+	MSG_POS_TX_ADDR,
+	MSG_POS_LEN,
+	MSG_POS_CMD,
+	MSG_POS_ARGS,
+};
+
+enum {
+	MSG_IDLE = 0,
+	MSG_RX_ADDR,
+	MSG_TX_ADDR,
+	MSG_LEN,
+	MSG_CMD,
+	MSG_ARGS,
+	MSG_READY,
+	MSG_ERROR,
+};
+
+struct rawMsg{
+	char data[BUFFER_SIZE];
+	char* head;
+	uint8_t status;
+	uint8_t remainingArgs;
+	uint8_t errorCount;
+};
+
+class RS485
+{
+	public:
+		RS485(std::string port, int baud);
+		~RS485();
+		void sendPacket(char * packet, unsigned int len);
+		void getPacket(char * packet);
+		void getMessege(char* msg);
+		void initBuffer();
+		int set_interface_attribs(int baud);
+		
+	private:
+		void sendChar(char ch, bool address = false);
+		int getChar(char &ch, bool &address);
+		bool parityCheck(char &ch, bool parity = false);
+		struct termios tio_;
+		int tty_fd_;
+		rawMsg buffer_array[1];
+		rawMsg* readInBuf;
+};
+
+#endif
