0cf7c265d523aa542676d0eb070807aed5361998
Jakob Kristensen <j4k0bk@gmail.com>
2015-04-18 11:47:37 +0200

    aosdjh2

--- Software/FlexPMS/KarContainer.cpp
+++ Software/FlexPMS/KarContainer.cpp
@@ -59,9 +59,10 @@ void KarContainer::reload() {
         kar->id = res->getInt("id");
         kar->name = res->getString("name");
         kar->address = res->getInt("address");
-        kar->ph = res->getInt("ph");
+        kar->ph = res->getDouble("ph");
         kar->volumen = res->getInt("volumen");
         kar->humidity = res->getInt("humidity");
+        
         kar->mwstatus = res->getBoolean("mwstatus");
         kar->ivalvestatus = res->getBoolean("ivalvestatus");
         kar->ovalvestatus = res->getBoolean("ovalvestatus");
