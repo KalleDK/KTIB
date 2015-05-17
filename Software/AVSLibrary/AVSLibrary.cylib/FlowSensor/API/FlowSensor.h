7eeecb07d31e82d8b4ac792b758b30b6c60f5d91
KasperSK <raizzat@gmail.com>
2015-05-17 10:45:27 +0200

    Added flowsensor

---
+++ Software/AVSLibrary/AVSLibrary.cylib/FlowSensor/API/FlowSensor.h
@@ -0,0 +1,26 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#if !defined(`$INSTANCE_NAME`_H_)
+#define `$INSTANCE_NAME`_H_
+    
+#include <cytypes.h>
+#include "..\..\Includes\avs_debug.h"
+
+void  `$INSTANCE_NAME`_Start();
+uint16 `$INSTANCE_NAME`_getFlowTicks(void);
+uint16 `$INSTANCE_NAME`_getFlowLiters(void);
+uint16 `$INSTANCE_NAME`_clacFlowLiters(void);
+    
+#endif
+
+/* [] END OF FILE */
